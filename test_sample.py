0c6e96089611cbc854136f8e4631fb81e22b93b6
vikasrana <viikasranax@gmail.com>
2026-07-11 22:03:09 +0530

    Initial commit of pytest setup

---
+++ test_sample.py
@@ -0,0 +1,12 @@
+def add(a, b):
+    return a + b
+
+
+def test_add_two_numbers():
+    result = add(2, 3)
+    assert result == 5
+
+
+def test_add_negative_numbers():
+    result = add(-1, -1)
+    assert result == -2
